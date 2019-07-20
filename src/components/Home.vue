e40e638c9a99f1bf6bd61134f458e96fc156567b
vozille <anwesh063@gmail.com>
2019-07-20 12:51:02 +0530

    home page done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,22 +5,55 @@
         <div class="my-auto">
           <h1 class="mb-0">Anwesh Mohanty</h1>
           <div class="subheading mb-5">Software Enginner, Musician, Forever Curious</div>
-
-          <!-- <div class="social-icons">
-            <a href="#">
-              <i class="fab fa-linkedin-in"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-github"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-twitter"></i>
-            </a>
-            <a href="#">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-          </div>-->
         </div>
+        <div id="typed-strings">
+          <p>
+            I am kinda obsessed with
+            <strong>Food</strong>.
+          </p>
+          <p>
+            I've learnt
+            <em>scala</em> ..
+            somehow
+          </p>
+          <p>
+            I play ..
+            <b>tabla</b>
+          </p>
+          <p>
+            In a love hate relationship with ..
+            <em>mongo-db</em>
+          </p>
+          <p>
+            The fact that the future is a distopya,
+            makes me appreciate how blessed I am,
+            right now
+          </p>
+          <p>
+            I love being around people,
+            <em>smarter than me</em>
+          </p>
+          <p>
+            Inspirations are everywhere.
+            You just need to change,
+            the way you look at things
+          </p>
+          <p>
+            I love statically typed languages,
+            python still has my heart though
+          </p>
+          <p>
+            The passion of every person,
+            has always left me
+            in awe
+          </p>
+          <p>
+            I am a backend engineer,
+            who can write passable front end,
+            and do devops occasionally.
+          </p>
+        </div>
+        <span id="typed"></span>
       </section>
     </div>
   </div>
@@ -28,13 +61,22 @@
 
 <script>
 import cards from "@/components/DescriptionCards";
+import Typed from "typed.js";
 export default {
   name: "Home",
-  created: function() {},
-  data() {
-    return {
-      msg: "Welcome to Your Vue.js App"
+  mounted: function() {
+    var options = {
+      stringsElement: "#typed-strings",
+      typeSpeed: 30,
+      loop: true,
+      fadeOut: true,
+      showCursor: false
     };
+
+    var typed = new Typed("#typed", options);
+  },
+  data() {
+    return {};
   },
   methods: {
     show_more_data: function() {
